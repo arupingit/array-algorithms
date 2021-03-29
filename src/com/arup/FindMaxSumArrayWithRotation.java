5592227982e3e2735f10cb9888a332bfb6a6ef4d
Arup Dutta <arupdutta@Arups-Air.fios-router.home>
2021-03-29 08:40:16 -0400

    Fix FindMaxSumArrayWithRotation

--- src/com/arup/FindMaxSumArrayWithRotation.java
+++ src/com/arup/FindMaxSumArrayWithRotation.java
@@ -3,14 +3,14 @@ package com.arup;
 public class FindMaxSumArrayWithRotation {
 
 	public static void main(String[] args){
-		int[] arr = {3,4,5,1,2};
+		int[] arr = {1, 20, 2, 10};
 		int n = arr.length;
 		int sum = findSum(arr);
 		int max = sum;
 		int arraysum = arraysum(arr);
 		
 		for(int i=1; i<n; i++){
-			sum = arraysum + sum - n*2;
+			sum = arraysum + sum - n*arr[n-i];
 			System.out.println(sum);
 			if(sum>max){
 				max = sum;
